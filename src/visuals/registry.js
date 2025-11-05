d7c99edd0cad557e9af22a1ea40324f73b21e87e
timfsus2ing <XiaoTianFan@users.noreply.github.com>
2025-11-05 21:21:04 +0400

    clear legacy

--- src/visuals/registry.js
+++ src/visuals/registry.js
@@ -1,19 +1,7 @@
-import { create as createHeatRing } from './factories/heatRing.js';
-import { create as createRibbonWave } from './factories/ribbonWave.js';
 import { create as createAccidentBursts } from './factories/accidentBursts.js';
 import { create as createFlashingCircularSpectrogram } from './factories/flashingCircularSpectrogram.js';
-import { create as createPulseStrokes } from './factories/pulseStrokes.js';
-import { create as createPolygonWrinkle } from './factories/polygonWrinkle.js';
-import { create as createStarburstShards } from './factories/starburstShards.js';
-import { create as createFlareHeat } from './factories/flareHeat.js';
-import { create as createSteamParticles } from './factories/steamParticles.js';
-import { create as createBubbleField } from './factories/bubbleField.js';
 import { create as createTapWaterSpectrogram } from './factories/tapWaterSpectrogram.js';
-import { create as createVortexSpiral } from './factories/vortexSpiral.js';
-import { create as createSprayCone } from './factories/sprayCone.js';
-import { create as createSparkLine } from './factories/sparkLine.js';
 import { create as createJuiceDroplets } from './factories/juiceDroplets.js';
-import { create as createMicrowaveGrid } from './factories/microwaveGrid.js';
 import { create as createFridgeDrop } from './factories/fridgeDrop.js';
 import { create as createKnifeStripe } from './factories/knifeStripe.js';
 import { create as createChopStripe } from './factories/chopStripe.js';
@@ -30,22 +18,10 @@ import { create as createGlassClinkRings } from './factories/glassClinkRings.js'
 import { create as createBoilingVerticalSine } from './factories/boilingVerticalSine.js';
 
 const registry = {
-  heatRing: createHeatRing,
-  ribbonWave: createRibbonWave,
   accidentBursts: createAccidentBursts,
   flashingCircularSpectrogram: createFlashingCircularSpectrogram
-  ,pulseStrokes: createPulseStrokes
-  ,polygonWrinkle: createPolygonWrinkle
-  ,starburstShards: createStarburstShards
-  ,flareHeat: createFlareHeat
-  ,steamParticles: createSteamParticles
-  ,bubbleField: createBubbleField
   ,tapWaterSpectrogram: createTapWaterSpectrogram
-  ,vortexSpiral: createVortexSpiral
-  ,sprayCone: createSprayCone
-  ,sparkLine: createSparkLine
   ,juiceDroplets: createJuiceDroplets
-  ,microwaveGrid: createMicrowaveGrid
   ,fridgeDrop: createFridgeDrop
   ,knifeStripe: createKnifeStripe
   ,chopStripe: createChopStripe
